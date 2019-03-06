49c26ef24d83b86170f81690b4ef2392b9290f70
Thesoundguy666 <44259701+Thesoundguy666@users.noreply.github.com>
2019-03-06 12:25:13 +0000

    Create LoadFromCSV.cs

---
+++ LoadFromCSV.cs
@@ -0,0 +1,53 @@
+﻿//-------------------------------
+// Created by Lee Elliott
+// 15/11/2018
+//
+// A script designed to load
+// a specific phrase from
+// a CSV file.
+//
+//-------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadFromCSV : MonoBehaviour
+{
+
+	// Use this for reading speech phrases
+	public string LoadSpeech(int row, int column)
+    {
+        TextAsset speechData = Resources.Load<TextAsset>("Speech");
+
+		// Split file into individual lines
+        string[] data = speechData.text.Split(new char[] { '~' });
+
+		// Split lines into individual phrases
+        string[] words = data[row].Split(new char[] { '|' });
+
+        // Return the phrase requested
+        if (column < words.Length)
+            return words[column];
+        else
+            return "Index out of range";
+    }
+
+    // Use this for generating names
+    public string LoadName(int row, int column)
+    {
+        TextAsset speechData = Resources.Load<TextAsset>("Names");
+
+        // Split file into individual lines
+        string[] data = speechData.text.Split(new char[] { '\n' });
+
+        // Split lines into individual phrases
+        string[] words = data[row].Split(new char[] { ',' });
+
+        // Return the phrase requested
+        if (column < words.Length)
+            return words[column];
+        else
+            return "Index out of range";
+    }
+}
