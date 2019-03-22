38a45e764de3b59e85087b1bcf31d3cbf8e9855a
Lee Elliott <leeelliott33@hotmail.co.uk>
2019-03-22 15:22:21 +0000

    Update
    Added to functionality of mission menu
    Currently bugged when switching from "main menu" no idea why

---
+++ Assets/Scripts/MissionButtonScript.cs
@@ -0,0 +1,31 @@
+﻿//-------------------------------
+// Created by Lee Elliott
+// 22/03/2019
+//
+// A script designed to hold all
+// functionality of the mission
+// buttons on the mission menu.
+//
+//-------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionButtonScript : MonoBehaviour
+{
+    // Mission number to be passed
+    public int missionNumber = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
